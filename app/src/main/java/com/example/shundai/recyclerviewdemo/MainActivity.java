548e6ce4b762f9248cdd21e21def62a2f86062d3
zhiwei <deva1213082309@gmail.com>
2017-10-09 17:33:31 +0800

    first commit

---
+++ app/src/main/java/com/example/shundai/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.shundai.recyclerviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRelv;
+    private MainAdapter mAdapter;
+    private List<String> mDatas=new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRelv = (RecyclerView) findViewById(R.id.rlv);
+        mRelv.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        LinearLayoutManager manager=new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRelv.setLayoutManager(manager);
+        mRelv.setAdapter(mAdapter=new MainAdapter(this,mDatas));
+        for (int i = 0; i < 20; i++) {
+            mDatas.add("这是我的第"+i+"条数据");
+        }
+
+        mAdapter.setOnItemClickListener(new MainAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View view, RecyclerView.ViewHolder vh) {
+                Toast.makeText(MainActivity.this, "点击事件"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mAdapter.setOnItemLongClickListener(new MainAdapter.OnItemLongClickListener() {
+            @Override
+            public void onItemLongClick(int position, View view, RecyclerView.ViewHolder vh) {
+                Toast.makeText(MainActivity.this, "长按事件"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/shundai/recyclerviewdemo/MainAdapter.java
@@ -0,0 +1,182 @@
+package com.example.shundai.recyclerviewdemo;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by win7 on 2017/10/9.
+ */
+
+public class MainAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    private static final int ITEM_TYPE_PROGRESS = 1;
+    private static final int ITEM_TYPE_IMAGE = 2;
+    private static final int ITEM_TYPE_STRING = 3;
+
+
+    private Context mContext;
+    private List<String> mDatas;
+    private OnItemClickListener onItemClickListener;
+    private final LayoutInflater inflater;
+    private OnItemLongClickListener onItemLongClickListener;
+
+    public MainAdapter(Context context, List<String> datas) {
+        mContext = context;
+        mDatas = datas;
+        inflater = LayoutInflater.from(context);
+    }
+
+    public Context getmContext() {
+        return mContext;
+    }
+
+    public void setmContext(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public List<String> getmDatas() {
+        return mDatas;
+    }
+
+    public void setmDatas(List<String> mDatas) {
+        this.mDatas = mDatas;
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if (ITEM_TYPE_PROGRESS == viewType) {
+            view = inflater.inflate(R.layout.item_image, null);
+            return new MyProgressViewholder(view);
+        } else if (ITEM_TYPE_STRING == viewType) {
+            view = inflater.inflate(R.layout.item_main, null);
+            return new MyViewHolder(view);
+        }
+        if (ITEM_TYPE_IMAGE == viewType) {
+            view = inflater.inflate(R.layout.item_image2, null);
+            return new MyImageViewHolder(view);
+        }
+
+        return null;
+
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int adapterPosition = holder.getAdapterPosition();
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(adapterPosition, view, holder);
+                }
+            }
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                int adapterPosition = holder.getAdapterPosition();
+                if (onItemLongClickListener != null) {
+                    onItemLongClickListener.onItemLongClick(adapterPosition, view, holder);
+                }
+                return true;
+            }
+        });
+
+        if (holder instanceof MyProgressViewholder) {
+            ((MyProgressViewholder) holder).tv1.setText("正在加载中...");
+        } else if (holder instanceof MyViewHolder) {
+            if (position>3&&position<mDatas.size()+1){
+                position=position-1;
+                ((MyViewHolder) holder).tv.setText(mDatas.get(position));
+            }else {
+                ((MyViewHolder) holder).tv.setText(mDatas.get(position));
+            }
+        } else if (holder instanceof MyImageViewHolder) {
+            ((MyImageViewHolder) holder).iv.setImageDrawable(ContextCompat.getDrawable(mContext, R.drawable.tongliya));
+        }
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size()+2;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tv = itemView.findViewById(R.id.tv_item);
+        }
+    }
+
+
+    public interface OnItemClickListener {
+        void onItemClick(int position, View view, RecyclerView.ViewHolder vh);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemLongClickListener {
+        void onItemLongClick(int position, View view, RecyclerView.ViewHolder vh);
+
+    }
+
+    public void setOnItemLongClickListener(OnItemLongClickListener onItemLongClickListener) {
+        this.onItemLongClickListener = onItemLongClickListener;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == mDatas.size() +1) {
+            return ITEM_TYPE_PROGRESS;
+        } else if (position == 4) {
+            return ITEM_TYPE_IMAGE;
+        } else {
+            return ITEM_TYPE_STRING;
+        }
+    }
+
+    static class MyProgressViewholder extends RecyclerView.ViewHolder {
+        @BindView(R.id.pb)
+        ProgressBar pb;
+        @BindView(R.id.tv1)
+        TextView tv1;
+
+        public MyProgressViewholder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    static class MyImageViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv)
+        ImageView iv;
+
+
+        public MyImageViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
